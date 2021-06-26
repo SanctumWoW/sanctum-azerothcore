df8c7710d17fb97779da1d0254be9446cfc61e92
Dokito <michauda16@gmail.com>
2021-06-26 14:13:11 -0400

    Gozzim/Dokito - Changed Talent Points calculation
    (cherry picked from commit e16eeb200a028d757520733d4038072342ca222e)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -13252,17 +13252,15 @@ void Player::StoreLootItem(uint8 lootSlot, Loot* loot)
 uint32 Player::CalculateTalentsPoints() const
 {
     uint32 base_talent = getLevel() < 10 ? 0 : getLevel() - 9;
+    uint32 custom_talent = std::min(getLevel() < 10 ? 0 : getLevel() - 9, 71);
 
-    if (getClass() != CLASS_DEATH_KNIGHT || GetMapId() != 609)
-        return uint32(base_talent * sWorld->getRate(RATE_TALENT));
-
-    uint32 talentPointsForLevel = getLevel() < 56 ? 0 : getLevel() - 55;
-    talentPointsForLevel += m_questRewardTalentCount;
+    uint32 talentPointsForLevel = custom_talent + m_questRewardTalentCount;
 
     if (talentPointsForLevel > base_talent)
-        talentPointsForLevel = base_talent;
-
+            talentPointsForLevel = base_talent;
+    
     talentPointsForLevel += m_extraBonusTalentCount;
+
     return uint32(talentPointsForLevel * sWorld->getRate(RATE_TALENT));
 }
 
