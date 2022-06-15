90c7fa73dc839e89ed0b52b341821a4642012046
Kempec Halk <80704304+Gozzim@users.noreply.github.com>
2022-06-16 01:34:21 +0200

    [SC] feat: 1v1 arenas (#1)
    feat: 1v1 arenas

--- src/server/game/Handlers/ArenaTeamHandler.cpp
+++ src/server/game/Handlers/ArenaTeamHandler.cpp
@@ -138,7 +138,7 @@ void WorldSession::HandleArenaTeamInviteOpcode(WorldPacket& recvData)
         return;
     }
 
-    if (arenaTeam->GetMembersSize() >= arenaTeam->GetType() * 2)
+    if (arenaTeam->GetMembersSize() >= arenaTeam->GetType() * 2 || (arenaTeam->GetType() == 5 && arenaTeam->GetMembersSize() >= 1))
     {
         SendArenaTeamCommandResult(ERR_ARENA_TEAM_CREATE_S, arenaTeam->GetName(), "", ERR_ARENA_TEAM_TOO_MANY_MEMBERS_S);
         return;
